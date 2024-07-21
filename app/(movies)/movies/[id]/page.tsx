6fccd8589a1369f505b010d7769983963adbdf27
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2024-07-22 00:02:51 +0900

    #3.4 Parallel Requests
    영화를 클릭하면 일어나는 일에 대해 알아보기 위해, page.tsx 파일을 생성하고 component를 async 함수로 만들고, fetch할 함수를 작성했다.
    
    확인해보면, fetch가 캐시되는 것을 알 수 있다. 이미 클릭한 영화에 대해 fetch했기 때문에 더 이상 로딩할 필요가 없다. 이제 영화의 id를 얻고, 예산이나 평가 등 세부사항을 알고 싶은데, 예를 들어, 예고편 영상을 fetch하는 함수를 만들었다.
    
    문제는 getMovie 함수와 getVideos 함수가 순차적으로 이루어진다는 것이다. 만약 getMovie가 시간이 오래 걸리면 getVideos에서 영상들을 가져오는데 시간이 적게 걸린다고 해도 여전히 모든게 오래 걸릴 것이다. 이런 문제를 극복하기 위해 함수를 병렬로 실행하는 과정이 필요했다.
    
    현재 console.log로 찍어보면, 순차적으로 실행하기 때문에 getMovie에서 3초, getVideos에서 3초, API 응답시간까지 해서 최소 6초 이상이 걸린다.
    
    병렬로 처리하기 위해 Promise.all을 사용했다. 이걸 사용하면 getMovie와 getVideos를 await할 거다. 동시에 말이다.
    
    Promise.all이 getMovie를 await하고 movie 값을 가져오고, getVideos를 await하고 videos 값을 가져올 것이다. 최종적으로 [movie, videos] array를 준다.
    
    실행해보면, 2개의 함수가 동시에 시작한 것을 확인할 수 있다. 모든 작업은 총 3초가 소요된다. 이게 병렬적으로 fetch하는 하나의 방법이다. 하지만 이보다 더 좋은 방법도 있다.

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,7 +1,30 @@
-export default function MovieDetail({
+import { API_URL } from "../../../(home)/page";
+
+export const metadata = {
+  title: "Movie Page",
+};
+
+async function getMovie(id: string) {
+  console.log(`Fetching Movies: ${Date.now()}`);
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  const response = await fetch(`${API_URL}/${id}`);
+  return response.json();
+}
+
+async function getVideos(id: string) {
+  console.log(`Fetching Videos: ${Date.now()}`);
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  const response = await fetch(`${API_URL}/${id}/videos`);
+  return response.json();
+}
+
+export default async function MovieDetail({
   params: { id },
 }: {
   params: { id: string };
 }) {
-  return <h1>Movie {id}</h1>;
+  console.log("Start fetching...");
+  const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
+  console.log("End fetching...");
+  return <h1>{movie.title}</h1>;
 }
