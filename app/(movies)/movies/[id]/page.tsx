6fd6b520a282bb1141ddb8194548f8adea2615fa
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2024-07-22 01:08:03 +0900

    #3.5 Suspense
    이제 Promise를 동시에 시작하는 법을 알았다. 이 방식으로 fetch하는 시간이 짧아졌고, 순차적이지 않게 되었다. 하지만 이 방식도 문제가 있는데 영화 제목이 보여지고 UI가 준비될 때까지 movie와 videos 둘 다 있어야 한다. getMovie와 getVidoes 함수가 둘 다 끝나야 한다는 것이다. 그래야 UI를 볼 수 있다.
    
    그래서 알아본 건, fetch 함수들을 분리하는 방법이다. 그리고 둘 다 끝날 때까지 기다려서 사용자가 UI를 보는 대신, getMovie가 준비되면 movie에 대한 UI를 보여줄거고, getVideos가 준비되면 videos를 보여줄거다. 둘 중 어느 한쪽도 기다릴 필요 없다.
    
    다시 상기해보자면, 이건 다른 방법일 뿐이다. 많은 데이터 소스에서 fetch해야 할 때 말이다. 가장 편한 방법은 page 파일에서 데이터를 fetch하는 거다. 왜냐하면 자동적으로 loading component가 화면에 생길거고 자동적으로 바뀔 것이기 때문이다. 편하다. 그냥 async를 넣고, 데이터를 fetch하고 loading 파일을 만들면 된다.
    
    하지만, 데이터 소스가 여러 개라면 Suspense를 써야 한다. 이를 위해 components 폴더에 movie-info, movie-videos 파일을 만들어서 컴포넌트를 분리했다. 두 컴포넌트 모두 async function이고 server component다. 그리고 자신에 관한 데이터만 fetch하고 있다. 이로써 각각의 파일에서 개별적으로 기다릴 수 있게 했다. page에서 2가지 모두 render할 건데, 개별적으로 기다릴 수 있다는 말이다.
    
    page에서 2개를 렌더링했다. 무슨 영화를 찾는지 알아야 하니 id도 알아야 하겠다. 이 컴포넌트들은 async이기 때문에 둘 다 await해줘야 한다. 그래서 Suspense로 감싸줘야 한다. Suspense component에는 fallback이라는 prop이 있고, 이건 component가 await되는 동안 표시할 메시지를 render할 수 있게 해준다. 기본적으로 다른 component를 render할 수 있는 거다.
    
    이제 화면을 확인해보면, 2개의 로딩이 생기는 것을 볼 수 있다. 하나는 Loading videos, 다른 하나는 Loading Info. 확인해보면 첫번째로 videos가 들어오고 그 다음 영화 정보가 들어온다. 병렬적으로 2가지를 동시에 fetch할 수 있는데, 하나의 요청이 완료되면 즉시 component가 render된다. 다른 component를 기다릴 필요가 없다.
    
    동시에 데이터를 가져오기 시작해서 데이터 로딩이 완료된 컴포넌트 순서대로 결과를 보여준다.

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,30 +1,25 @@
+import { Suspense } from "react";
 import { API_URL } from "../../../(home)/page";
+import MovieInfo from "../../../../components/movie-info";
+import MovieVideos from "../../../../components/movie-videos";
 
 export const metadata = {
   title: "Movie Page",
 };
-
-async function getMovie(id: string) {
-  console.log(`Fetching Movies: ${Date.now()}`);
-  await new Promise((resolve) => setTimeout(resolve, 3000));
-  const response = await fetch(`${API_URL}/${id}`);
-  return response.json();
-}
-
-async function getVideos(id: string) {
-  console.log(`Fetching Videos: ${Date.now()}`);
-  await new Promise((resolve) => setTimeout(resolve, 3000));
-  const response = await fetch(`${API_URL}/${id}/videos`);
-  return response.json();
-}
 
 export default async function MovieDetail({
   params: { id },
 }: {
   params: { id: string };
 }) {
-  console.log("Start fetching...");
-  const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
-  console.log("End fetching...");
-  return <h1>{movie.title}</h1>;
+  return (
+    <div>
+      <Suspense fallback={<h1>Loading movie info</h1>}>
+        <MovieInfo id={id} />
+      </Suspense>
+      <Suspense fallback={<h1>Loading movie videos</h1>}>
+        <MovieVideos id={id} />
+      </Suspense>
+    </div>
+  );
 }
---
+++ components/movie-videos.tsx
@@ -0,0 +1,14 @@
+import { API_URL } from "../app/(home)/page";
+
+async function getVideos(id: string) {
+  console.log(`Fetching Videos: ${Date.now()}`);
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  const response = await fetch(`${API_URL}/${id}/videos`);
+  return response.json();
+}
+
+export default async function MovieVideos({ id }: { id: string }) {
+  //  이건 videos에 관한 UI만을 가질거다.
+  const videos = await getVideos(id);
+  return <h6>{JSON.stringify(videos)}</h6>;
+}
