db0ca087c6c5ff065608c542377fe07ab2693aac
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2024-07-22 10:45:55 +0900

    #3.7 Error Handling
    만약 영화 정보가 로드되지 않거나, API 연결이 끊어지는 등 오류가 발생할 때의 방법을 알아봤다.
    
    일단 MovieVideos의 getVideos에서 3초간 기다리고 에러를 발생시키도록 했다. 그리고 나서 영화를 클릭하면 3초 동안은 fetching하다가 에러가 발생하는 것을 확인할 수 있다. 사용자는 아무것도 없는 화면만 보게 될텐데, 적어도 사용자가 다른 페이지로 돌아갈 수 있도록 했으면 좋겠다는 생각을 했다.
    
    그래서 [id] 폴더에 error 파일을 만들었다. 다시 영화를 클릭해보면 Loading... 텍스트가 떴다가 이 페이지만 에러가 발생한 것을 볼 수 있다. 다른 작업, 예를 들어 About Us 페이지로 이동하거나 Home으로 이동하는 것은 원활하게 작동된다.
    
    이처럼 error 파일만 만들어 놓으면 자동으로 page 혹은 page의 자식 페이지(지금은 MovieVideos)에서 에러가 발생한다면 페이지는 동작하지 않을 거다.
    
    정리하자면, 페이지가 작동하지 않을 수는 있다. 그렇더라도 어플리케이션 전체적으로는 정상적으로 작동되야 할 것이다. 내가 해야할 건 단지 error 파일을 page 옆에 만드는 일이다. 단, 이 error 파일은 현재 /movies/id 에만 적용된다는 사실을 알아야 한다. error 파일은 바로 옆에 있는 page에서 발생하는 에러에만 작동하는 것이다. 가령, 홈페이지에서 에러가 발생한다면, 막을 수 없다. 에러를 처리하고 싶다면 홈페이지 옆에 error 파일을 생성해줘야 한다는 것이다.

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -3,8 +3,12 @@ import { API_URL } from "../app/(home)/page";
 async function getVideos(id: string) {
   console.log(`Fetching Videos: ${Date.now()}`);
   await new Promise((resolve) => setTimeout(resolve, 3000));
-  const response = await fetch(`${API_URL}/${id}/videos`);
-  return response.json();
+
+  //  에러 임의 발생
+  throw new Error("Something broke...");
+
+  /*const response = await fetch(`${API_URL}/${id}/videos`);
+  return response.json();*/
 }
 
 export default async function MovieVideos({ id }: { id: string }) {
