f41f71b3d39467a35023c7876a680ce4450648f2
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2024-07-23 16:01:48 +0900

    #4.2 Movie Styles
    리팩토링 & 스타일 부여
    
    components 안에 components를 위한 폴더들을 생성하고, 그 안에 컴포넌트와 컴포넌트 모듈 스타일을 조직화하는 방식으로 리팩토링했다.
    
    추가로, movie.tsx 파일에서 next navigation 모듈의 useRouter hook을 사용하고 push 메서드를 이용해서 클릭한 영화의 URL로 이동시켰다.
    
    useRouter hook과 onClick EventListener가 사용되므로 use client 지시어도 최상단에 추가했다.

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -2,13 +2,12 @@ import { API_URL } from "../app/(home)/page";
 
 async function getMovie(id: string) {
   console.log(`Fetching Movies: ${Date.now()}`);
-  await new Promise((resolve) => setTimeout(resolve, 3000));
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   const response = await fetch(`${API_URL}/${id}`);
   return response.json();
 }
 
 export default async function MovieInfo({ id }: { id: string }) {
-  //  이건 movie에 관한 UI만을 가질거다.
   const movie = await getMovie(id);
   return <h6>{JSON.stringify(movie)}</h6>;
 }
--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -2,17 +2,12 @@ import { API_URL } from "../app/(home)/page";
 
 async function getVideos(id: string) {
   console.log(`Fetching Videos: ${Date.now()}`);
-  await new Promise((resolve) => setTimeout(resolve, 3000));
-
-  //  에러 임의 발생
-  throw new Error("Something broke...");
-
-  /*const response = await fetch(`${API_URL}/${id}/videos`);
-  return response.json();*/
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const response = await fetch(`${API_URL}/${id}/videos`);
+  return response.json();
 }
 
 export default async function MovieVideos({ id }: { id: string }) {
-  //  이건 videos에 관한 UI만을 가질거다.
   const videos = await getVideos(id);
   return <h6>{JSON.stringify(videos)}</h6>;
 }
